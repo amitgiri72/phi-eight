ecd7253ec5b40393abec9b45c44a8004e16c39a3
amitgiri72 <ag4982324@gmail.com>
2024-08-06 11:51:00 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Faq from "./component/faq/Faq";
+import Footer from "./component/footer/Footer";
+import Header from "./component/header/Header";
+import PartnerDriverPage from "./component/hero/PartnerDriver";
+import Hiw from "./component/hiw/Hiw";
+import Join from "./component/join/Join";
+import Testimonials from "./component/testimonial/Testimonial";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <PartnerDriverPage />
+      <Hiw />
+      <Testimonials />
+      <Faq />
+      <Join />
+      <Footer />
     </div>
   );
 }
